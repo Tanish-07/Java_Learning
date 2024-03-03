14ca0a0381012ca58d55af422973d5598814cf12
Tanish <tanish14082004@gmail.com>
2024-03-03 17:18:13 +0530

    First Push

---
+++ src/PracticeSet1.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class PracticeSet1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//
+//        //    WAP to sum 3 no in java:
+//        System.out.print("Please enter no:");
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//        int sum = a+b+c;
+//        System.out.println("Sum of the no:"+sum);
+//
+//        //WAP to calculate CGPA using marks of 3 subjects.
+//        System.out.println("Please enter your marks:");
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//        int sum = a+b+c;
+//        int cgpa = sum/3;
+//        System.out.println("CGPA of student:"+cgpa);
+//
+//        //WAP TO ADD NAME IN LINE "HELLO <NAME>,HAVE A GOOD DAY"
+//        System.out.println("Please enter your name:");
+//        String name =sc.nextLine();
+//        System.out.println("Hello"+name+"have a good day.");
+//
+//        //WAP TO CONVERT KM INTO MILES.
+//        System.out.println("Enter distance in Km:");
+//        float dis= sc.nextFloat();
+//        double miles = dis*0.6214;
+//        System.out.println("Your distance in miles:"+miles);
+//
+//        //WAP TO DETERMINE WHETHER ENTERED NO IS INTEGER OR NOT.
+//        System.out.println("Enter your no:");
+//        System.out.println(sc.hasNextInt());
+    }
+
+}
